2edc43712a93f49e137f3bcc71c1aed7032234f8
David <davidsanchezr81@gmail.com>
2018-03-11 21:28:45 +0000

    Hotel section added

--- src/main/java/HotelCalifornia/Hotel.java
+++ src/main/java/HotelCalifornia/Hotel.java
@@ -1,4 +1,41 @@
 package HotelCalifornia;
 
+import HotelCalifornia.RoomType.Bedroom;
+import HotelCalifornia.RoomType.Conferenceroom;
+
+import java.util.ArrayList;
+
 public class Hotel {
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<Conferenceroom> conferencerooms;
+
+
+    public Hotel(String name, ArrayList<Bedroom> bedrooms, ArrayList<Conferenceroom> conferenceRooms) {
+        this.name = name;
+        this.bedrooms = bedrooms;
+        this.conferencerooms = conferencerooms;
+
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return this.bedrooms;
+    }
+
+    public ArrayList<Conferenceroom> getConferencerooms() {
+        return this.conferencerooms;
+    }
+
+    public void addBedroom(Bedroom bedroom){bedrooms.add(bedroom);
+
+    }
+
+    public void addConferenceRoom(Conferenceroom conferenceroom){conferencerooms.add(conferenceroom);
+
+    }
+
+    public int countConferenceRoom(){return this.conferencerooms.size();}
+
+    public int countBedRoom(){return this.bedrooms.size();}
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,64 @@
+import HotelCalifornia.Guest;
+import HotelCalifornia.Hotel;
+import HotelCalifornia.Room;
+import HotelCalifornia.RoomType.Bed;
+import HotelCalifornia.RoomType.Bedroom;
+import HotelCalifornia.RoomType.Conferenceroom;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+    Hotel hotel;
+    Room room;
+    Bedroom bedroom;
+    Conferenceroom conferenceroom;
+    ArrayList<Bedroom> bedrooms;
+    ArrayList<Conferenceroom> conferencerooms;
+    ArrayList<Guest> guests;
+    Guest guest6;
+
+    @Before
+    public void before(){
+        guests = new ArrayList<>();
+        guest6 = new Guest("Monica");
+        bedroom = new Bedroom("Bedroom", guests, Bed.Single );
+        conferenceroom = new Conferenceroom("Conference", guests, "Tatanga");
+        bedrooms = new ArrayList<Bedroom>();
+//        bedrooms.add(bedroom);
+        conferencerooms = new ArrayList<Conferenceroom>();
+//        conferencerooms.add(conferenceroom);
+        hotel = new Hotel("Hotel California", bedrooms, conferencerooms);
+    }
+
+    @Test
+    public void canGetConfRoom(){
+        assertEquals(Arrays.asList(conferenceroom), hotel.getConferencerooms());
+    }
+//
+//    @Test
+//    public void getBedRooms(){
+//        assertEquals(Arrays.asList(bedroom), hotel.getConferencerooms());
+//    }
+
+    @Test
+    public void canAddBedRoom() {
+        hotel.addBedroom(bedroom);
+//        assertEquals(Arrays.asList(bedroom), hotel.getBedrooms());
+        assertEquals(1, hotel.countBedRoom());
+    }
+
+    @Test
+    public void canAddConfRoom() {
+        hotel.addConferenceRoom(conferenceroom);
+//        assertEquals(Arrays.asList(conferenceroom), hotel.getConferencerooms());
+        assertEquals(1, hotel.countConferenceRoom());
+
+    }
+
 }
